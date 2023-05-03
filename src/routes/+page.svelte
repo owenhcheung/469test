a972c34be2e476d15012955a7eb60254dde19838
Owen Cheung <ocheung198@gmail.com>
2023-05-03 11:02:58 -0700

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<div class="container mx-auto flex justify-center">
+    <h1 class="text-3xl font-bold">
+        Hello world!
+    </h1>
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    colors: {},
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
