17313a8171568e35c2f62d6dd0d4d78747c9bf3b
Owen <ocheung198@gmail.com>
2023-05-11 22:39:51 -0700

    aaa

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,19 @@
-<div class="container mx-auto flex justify-center">
-    <h1>test</h1>
-
-    <svg width="100" height="100">
-        <path
-          fill="cornflowerblue"
-          d={arcPath}
-          style="transform: translate(50%, 50%)"
-        />
-    </svg>
-
-    
-
-    
-</div>
 
 <script>
     import * as d3 from "d3";
-
-    const arcPathGenerator = d3.arc()
-        .innerRadius(25)
-        .outerRadius(40)
-        .startAngle(0)
-        .endAngle(5.5)
-        .padAngle(0)
-        .cornerRadius(20)
-    const arcPath = arcPathGenerator()
+    
 
 </script>
+
+<div class="h-screen w-screen flex p-10">
+    <div class="w-3/5 h-full">
+      <!-- D3 map goes here -->
+    </div>
+    <div class="w-2/5 h-full bg-gray-200 flex items-center justify-center">
+      <div>
+        <h2 class="text-4xl font-bold mb-4">An Understanding of the Digital Access Divide Outside of Seattle</h2>
+        <p class="text-lg pb-10">by Owen Cheung, Shirley Hu, Truong Le, Jason Lim</p>
+        <p class="text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      </div>
+    </div>
+  </div>
